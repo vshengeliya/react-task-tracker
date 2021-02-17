cd6eecaa86c72a0b25e4da0e97a2344002526cb9
Veronika Dodda <v.shengeliya@gmail.com>
2021-02-17 13:55:58 -0500

    before backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+
+  const [showAddTask, setShowAddTask]= useState(false)
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(()=>{
+   const getTasks = async()=>{
+     const tasksFromSever = await fetchTasks()
+     setTasks(tasksFromSever)
+   }
+   getTasks()
+  }, [])
+
+  //Fetch data from 
+
+  const fetchTasks = async() => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
+  //Add Task
+  const addTask =(task) =>{
+    const id = Math.floor(Math.random()* 10000) + 1
+    const newTask   = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  //Delete Task
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter((task)=> task.id !== id))
+  
+  }
+
+  //Toggle reimder
+  const toggleReminder = (id) =>{
+    setTasks(tasks.map((task) => 
+      task.id === id? {...task, reminder: !task.reminder}: task
+      )
+    )
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello from React</h1>
+        <Header onAdd={()=>setShowAddTask(!showAddTask)} 
+        showAdd={showAddTask}/>
+        {showAddTask && <AddTask onAdd={addTask}/>}
+        {tasks.length > 0? <Tasks tasks={tasks} onDelete={deleteTask} onToggle = {toggleReminder}/> 
+        : ('No task to Show')}
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text, onClick}) => {
+
+    return (
+        <div>
+              <button onClick={onClick} style={{backgroundColor:color}} className='btn'>{text}</button>
+        </div>
+    )
+}
+
+Button.defaulProps= {
+    color: 'sleetblue'
+}
+
+Button.propsTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+export default Button
